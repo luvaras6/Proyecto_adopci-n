4b8ab2b0e8482b9eed14933d5baff643351a2afc
luvaras6 <lucia.varas6@gmail.com>
2023-01-25 19:21:31 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,56 @@
+let animales = [];
+
+data.forEach((data) => {
+    animales += `
+    <div>
+        <div class="contenedor-img">
+            <img src="${data.img}">
+        </div>
+        <div>
+            <h2 class = "nombre">${data.nombre}</h2>
+            <p class = "descripcion">Tiene ${data.edad_aprox}. Cuenta con ${data.vacunas} las vacunas</p>
+        </div>
+    </div>
+    `
+});
+
+document.querySelector(".carrousel").innerHTML = animales;
+
+
+$(function () {
+    $(".responsive").slick({
+        dots: true,
+        infinite: false,
+        speed: 300,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+        ]
+    })
+});
